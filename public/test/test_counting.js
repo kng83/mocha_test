55036af3719ce31e87b8749c4d6d071fff509bf9
kng83 <pawel.kng@gmail.com>
2017-12-21 20:20:26 +0100

    initial commit

---
+++ public/test/test_counting.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var assert = require("assert");
+describe('Test 1', function () {
+    it('This value should be equal 2', function () {
+        assert(2 === 2);
+    });
+    it('This value should be equal 3', function () {
+        assert(3 === 3);
+        console.log('Test 2');
+    });
+});
---
+++ test/test_counting.ts
@@ -0,0 +1,11 @@
+import * as assert from 'assert';
+
+describe('Test 1',()=>{
+  it('This value should be equal 2', ()=>{
+    assert(2 === 2);
+  });
+    it('This value should be equal 3', ()=>{
+      assert(3 === 3);
+      console.log('Test 2')
+    });
+});
