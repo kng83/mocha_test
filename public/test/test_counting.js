1b9986a6c4fde68deceb3461b6291d1346bf3d75
unknown <pawel.kng@gmail.com>
2017-12-21 23:46:17 +0100

    chai commit

--- public/test/test_counting.js
+++ public/test/test_counting.js
@@ -1,12 +1,25 @@
 "use strict";
 exports.__esModule = true;
 var assert = require("assert");
+require("chai/register-should");
 describe('Test 1', function () {
     it('This value should be equal 2', function () {
         assert(2 === 2);
     });
     it('This value should be equal 3', function () {
         assert(3 === 3);
-        console.log('Test 2');
+    });
+    it('This value shold be equal 4', function () {
+        assert.equal(4, 4);
+    });
+    it('This value should be equal 5', function () {
+        assert.equal([1, 2, 3].indexOf(2), 1);
+    });
+    it('This is my chai value', function () {
+        [1, 2, 3, 5, 6].indexOf(5).should.be.equal(3);
+    });
+    it('This name should be a length at least of 4', function () {
+        var name = 'Pawel';
+        name.should.have.length.above(3);
     });
 });
--- test/test_counting.ts
+++ test/test_counting.ts
@@ -1,11 +1,27 @@
 import * as assert from 'assert';
+import 'chai/register-should';
+
 
 describe('Test 1',()=>{
-  it('This value should be equal 2', ()=>{
-    assert(2 === 2);
-  });
+    it('This value should be equal 2', ()=>{
+      assert(2 === 2);
+    });
     it('This value should be equal 3', ()=>{
       assert(3 === 3);
-      console.log('Test 2')
     });
+    it('This value shold be equal 4', () =>{
+        assert.equal(4,4);
+    });
+
+    it('This value should be equal 5', ()=>{
+      assert.equal([1,2,3].indexOf(2),1);
+    })
+
+    it('This is my chai value', ()=>{
+        [1,2,3,5,6].indexOf(5).should.be.equal(3);
+    });
+    it('This name should be a length at least of 4', ()=>{
+        let name ='Pawel';
+        name.should.have.length.above(3);
+    })
 });
