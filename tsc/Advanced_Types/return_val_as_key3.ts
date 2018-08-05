0c1ce9d576dc871ec4440823234daca8c30b5b68
kng83 <pawel.kng@gmail.com>
2018-08-05 16:24:24 +0200

    rede

--- tsc/Advanced_Types/return_val_as_key3.ts
+++ tsc/Advanced_Types/return_val_as_key3.ts
@@ -6,15 +6,13 @@ class UCtrl{
     age = 5;
 }
 class DCtrl{
-    wname= 'Piotrek';
-    wage = 5;
+    name= 'Piotrek';
+    age = 5;
 }
 
 
 type D1<K> = {[Key in keyof K]:(req:any,res:any)=>any | void}
 type D2<T extends new()=>any> = D1<InstanceType<T>>
-
-
 
 
 interface ValueT<K extends string,T extends any> {
@@ -29,17 +27,18 @@ type VirtualObj<K extends string,T extends any> = { [Key in K]:T}
 function getByN<K extends string,T extends any>(valueArr: ValueT<K,T>[]): VirtualObj<K,T> {
     let out = {} as VirtualObj<K, T>;
     valueArr.forEach((value:ValueT<K,T>) => {
-        out[value.controllerName] = value.className();
+
+        out[value.controllerName] = value.className;
     });
     return out
 }
 
 let ab = getByN([
-    { controllerName: 'poko',className: new UCtrl() },
+    { controllerName: 'poko',className: new DCtrl() },
     { controllerName: 'miko',className:new UCtrl() }
     ]);
+ab.
 
-//a.miko
 
 
 
