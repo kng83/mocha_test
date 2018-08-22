6fc1f36be39025e7766c3496757febe1058beebb
kng83 <pawel.kng@gmail.com>
2018-08-22 18:19:46 +0200

    some

---
+++ tsc/Other_Types/bind_function_with_class.ts
@@ -0,0 +1,34 @@
+class UserService10 {
+    user = 'user';
+    constructor(animal: string) {
+        this.user = this.user + animal;
+    }
+}
+
+class MessageService10 {
+    message = 'message'
+    constructor(animal: string) {
+        this.message = this.message + animal;
+    }
+}
+
+
+function makingServices<T extends any, K extends { [Key in keyof T]: InstanceType<T[Key]> }>(servicesObj: T) {
+    return createServices.bind(null, servicesObj) as (db:string)=> K
+}
+
+let dbKot = 'kot';
+
+
+function createServices<T extends any, K extends { [Key in keyof T]: InstanceType<T[Key]> }>(servicesObj: T, dbKot) {
+    let output = {} as K;
+    for (let k in servicesObj) {
+        output[k] = new servicesObj[k](dbKot);
+    }
+    return output
+}
+
+
+let services = makingServices({ userService: UserService10, messageService: MessageService10 })(dbKot);
+console.log(services);
+s
